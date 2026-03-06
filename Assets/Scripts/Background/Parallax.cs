be08ed0b3ea2c871b727d5ca01d7ad680099dd88
Reimu <mfr98@uclive.ac.nz>
2026-03-06 18:55:25 +1300

    parallax working

--- Assets/Scripts/Background/Parallax.cs
+++ Assets/Scripts/Background/Parallax.cs
@@ -9,7 +9,9 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     [SerializeField] private List<SpriteRenderer> _backgroundLayers = new();
-    [SerializeField] private Dictionary<SpriteRenderer, float> _bgLayerStartingPos = new();
+    [SerializeField] private int loopBackgroundCount = 3;
+
+    private Dictionary<SpriteRenderer, float> _bgLayerStartingPos = new();
     private Camera _camera;
     private int _furthestLayer = 0;
     private int _lengthsTravelled = 0; // remind ben to add to his
@@ -38,31 +40,34 @@ public class Parallax : MonoBehaviour
     }
 
     //move backgrounds porportional to their sort order
-    void HandleParralax()
+    void HandleParallax()
     {
         foreach (SpriteRenderer layer in _backgroundLayers)
         {
+            // we want linearity
+            float parallaxFormula = (float)(_furthestLayer - layer.sortingOrder + 1) / _furthestLayer;
+
             //fractional movement with further sorting orders travelling faster. things move at a fraction of the camera's movement relative to distance
-            float lengthtraversal = _camera.transform.position.x * (1 - 1 / (float)layer.sortingOrder);
-            float dist = _camera.transform.position.x * (1 / (float)layer.sortingOrder);
+            float lengthtraversal = _camera.transform.position.x * (1 - parallaxFormula);
+            float dist = _camera.transform.position.x * parallaxFormula;
             layer.transform.position = new(_bgLayerStartingPos[layer] + dist, layer.transform.position.y, layer.transform.position.z);
 
             //check if the length traversed by this layer is longer then the size of the art layer
-            if (lengthtraversal > _bgLayerStartingPos[layer] + layer.bounds.size.x)
+            if (lengthtraversal > _bgLayerStartingPos[layer] + layer.size.x)
             {
-                _bgLayerStartingPos[layer] += layer.bounds.size.x;
+                _bgLayerStartingPos[layer] += layer.size.x * loopBackgroundCount;
             }
             //check to see if we have done so in the other direction
-            else if (lengthtraversal < _bgLayerStartingPos[layer] - layer.bounds.size.x)
+            else if (lengthtraversal < _bgLayerStartingPos[layer] - layer.size.x)
             {
-                _bgLayerStartingPos[layer] -= layer.bounds.size.x;
+                _bgLayerStartingPos[layer] -= layer.size.x * loopBackgroundCount;
             }
         }
     }
 
     void Update()
     {
-        HandleParralax();
+        HandleParallax();
     }
 
 
