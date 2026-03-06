057a7196f64380f5bfc231b551d9863faedd958a
beef.com <you@example.com>
2026-03-06 20:00:02 +1300

    chain parry, first few levels sorted.

---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    private Vector3 Offset;
+    [SerializeField] private Transform Quavo;
+    [SerializeField] private float TakeOff;
+    private Vector2 Migos = Vector2.zero;
+
+
+    private void Awake()
+    {
+        Offset = transform.position - Quavo.position;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 TargetPosition = Quavo.position + Offset;
+        TargetPosition.y = 0;
+
+        transform.position = Vector2.SmoothDamp(transform.position, TargetPosition, ref Migos, TakeOff);
+    }
+}
