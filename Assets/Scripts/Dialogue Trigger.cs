dc6ba179f5839a5034f6b623edc0159a23846c85
Reimu <mfr98@uclive.ac.nz>
2026-03-09 16:32:07 +1300

    dialouge trigger test

--- Assets/Scripts/Dialogue Trigger.cs
+++ Assets/Scripts/Dialogue Trigger.cs
@@ -31,7 +31,7 @@ public class DialogueTrigger : MonoBehaviour
             PlayerController playerController = other.GetComponentInParent<PlayerController>();
             if (playerController != null)
             {
-
+                Debug.Log("Instantied");
                 PlayDialogue(DialogueEvent);
             }
             else
